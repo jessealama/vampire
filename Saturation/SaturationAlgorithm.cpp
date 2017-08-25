7904b96d517d773e439d3939df61192d896db538
Giles <--global>
2017-08-25 22:23:35 +0100

    update

--- Saturation/SaturationAlgorithm.cpp
+++ Saturation/SaturationAlgorithm.cpp
@@ -793,7 +793,8 @@ void SaturationAlgorithm::addUnprocessedClause(Clause* cl)
     return;
   }
 
-  if (cl->isEmpty()) {
+  //if (cl->isEmpty() || (_answerLiteralManager && _answerLiteralManager->isAnswerClause(cl,true))){
+  if(cl->isEmpty()){
     handleEmptyClause(cl);
     return;
   }
@@ -813,7 +814,7 @@ void SaturationAlgorithm::addUnprocessedClause(Clause* cl)
 void SaturationAlgorithm::handleEmptyClause(Clause* cl)
 {
   CALL("SaturationAlgorithm::handleEmptyClause");
-  ASS(cl->isEmpty());
+  ASS(cl->isEmpty() || (_answerLiteralManager && _answerLiteralManager->isAnswerClause(cl,true)));
 
   if (isRefutation(cl)) {
     onNonRedundantClause(cl);
@@ -1161,6 +1162,12 @@ void SaturationAlgorithm::doOneAlgorithmStep()
   doUnprocessedLoop();
 
   if (_passive->isEmpty()) {
+
+    if (_answerLiteralManager){ 
+      _answerLiteralManager->print(); 
+      System::terminateImmediately(1);
+    }
+
     MainLoopResult::TerminationReason termReason =
 	isComplete() ? Statistics::SATISFIABLE : Statistics::REFUTATION_NOT_FOUND;
     MainLoopResult res(termReason);
